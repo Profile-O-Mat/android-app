fbf9bd4ad9d1f6b3c484be560b77b082a805bbcb
YoshisWelt <torben.wetter@t-online.de>
2017-07-02 14:07:48 +0200

    Added twittername-exists method

--- app/src/main/java/de/torbenwetter/profile_o_mat/Main.java
+++ app/src/main/java/de/torbenwetter/profile_o_mat/Main.java
@@ -10,6 +10,13 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main extends AppCompatActivity {
 
@@ -37,10 +44,29 @@ public class Main extends AppCompatActivity {
             public void onClick(View view) {
                 final String username = input.getText().toString();
                 if (!username.isEmpty()) {
-                    final Intent intent = new Intent(Main.this, SelectTwitter.class);
-                    intent.putExtra("username", username);
-                    intent.putExtra("direction", "forwards");
-                    startActivity(intent);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (twitterNameExists(username)) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        final Intent intent = new Intent(Main.this, SelectTwitter.class);
+                                        intent.putExtra("username", username);
+                                        intent.putExtra("direction", "forwards");
+                                        startActivity(intent);
+                                    }
+                                });
+                            } else {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(Main.this, "The username doesn't exist.", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        }
+                    }).start();
                 }
             }
         });
@@ -50,6 +76,48 @@ public class Main extends AppCompatActivity {
             overridePendingTransition(R.anim.to_right_in, R.anim.to_right_out);
         else
             overridePendingTransition(R.anim.to_left_in, R.anim.to_left_out);
+    }
+
+    private Boolean twitterNameExists(String twitterName) {
+        final String pageContent = getPageContent("https://twitter.com/" + twitterName);
+        if (pageContent != null) {
+            final String title = pageContent.substring(pageContent.indexOf("<title>") + "<title>".length(), pageContent.indexOf("</title>"));
+            return !title.equals("Twitter / ?");
+        }
+        return false;
+    }
+
+    private String getPageContent(String link) {
+        try {
+            final BufferedReader br = getBufferedReader(link);
+            if (br != null) {
+                String inputLine;
+                final StringBuilder content = new StringBuilder();
+                while ((inputLine = br.readLine()) != null)
+                    content.append(inputLine).append("\n");
+                br.close();
+                return content.toString();
+            } else
+                return null;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private BufferedReader getBufferedReader(String link) {
+        try {
+            final URL url = new URL(link);
+            final HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+            return new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 
     private String getDirection(Bundle savedInstanceState, Activity activity) {
